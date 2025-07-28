1ab633497f2d6eb68667ffe85ce2932c9c774ea6
danial-baraty <daniall.barati@gmail.com>
2025-07-29 00:01:28 +0330

    Use clearer names for merge pointers

--- merge_sort.py
+++ merge_sort.py
@@ -10,18 +10,18 @@ def merge_sort(arr: list[int]) -> list[int]:
 
     # Merge the two sorted halves
     sorted_array = []
-    left_ptr = right_ptr = 0
+    left_half_ptr = right_half_ptr = 0  # clearer pointer names
 
-    while left_ptr < len(left_half) and right_ptr < len(right_half):
-        if left_half[left_ptr] <= right_half[right_ptr]:
-            sorted_array.append(left_half[left_ptr])
-            left_ptr += 1
+    while left_half_ptr < len(left_half) and right_half_ptr < len(right_half):
+        if left_half[left_half_ptr] <= right_half[right_half_ptr]:
+            sorted_array.append(left_half[left_half_ptr])
+            left_half_ptr += 1
         else:
-            sorted_array.append(right_half[right_ptr])
-            right_ptr += 1
+            sorted_array.append(right_half[right_half_ptr])
+            right_half_ptr += 1
 
     # Append any remaining elements from either half
-    sorted_array.extend(left_half[left_ptr:])
-    sorted_array.extend(right_half[right_ptr:])
+    sorted_array.extend(left_half[left_half_ptr:])
+    sorted_array.extend(right_half[right_half_ptr:])
 
     return sorted_array
